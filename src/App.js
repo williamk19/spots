3dc96c42e9d915d7c878b7b67a36c22cea751c12
williamk19 <williamkurniawan1144@gmail.com>
2022-03-30 15:52:51 +0700

    module-3-homework-2 Fetch spotify api, search, and auth

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
+import React from 'react';
 import HomePage from './pages/home/home.page';
 import './App.css';
+const authUrl = `https://accounts.spotify.com/authorize?response_type=token&client_id=${process.env.REACT_APP_SPOTIFY_API_KEY}&scope=playlist-modify-private&redirect_uri=http://localhost:3000/`;
+const searchUrl = `https://api.spotify.com/v1/search?type=album&include_external=audio`;
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage/>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      access_token: '',
+      searchQuery: '',
+      data: {}
+    };
+  }
+
+  componentDidMount() {
+    let hash = window.location.hash.substring(1);
+    let params = {};
+    hash.split('&').map(hk => {
+      let temp = hk.split('=');
+      params[temp[0]] = temp[1];
+    });
+    this.setState({ access_token: params.access_token }, () => {
+      console.log(this.state.access_token);
+    });
+  }
+
+  onSearchChange = (e) => {
+    this.setState({ searchQuery: e.target.value }, () => {
+      console.log(this.state.searchQuery);
+    });
+  };
+
+  callApi = (e) => {
+    e.preventDefault();
+    fetch(`https://api.spotify.com/v1/search?q=${this.state.searchQuery}&type=track`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${this.state.access_token}`
+      },
+    })
+      .then(res => res.json())
+      .then(data => this.setState({ data: data }, () => console.log(this.state.data)));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <HomePage authUrl={authUrl} searchUrl={searchUrl} onSearchChange={this.onSearchChange} callApi={this.callApi} data={this.state.data} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import data from '../../data/data';
+// import data from '../../data/data';
 import './home.page.css';
 import SongComponent from '../../components/song/song.component';
 
-const songComponent = () => {
+const songComponent = ({ authUrl, onSearchChange, callApi, data }) => {
   return (
     <div className='container'>
       <h1 className='header'>Spots Music Playlist</h1>
-      {data.map((d) => (
+      <a className='links' href={authUrl}>
+        Sign In With Spotify
+      </a>
+      <input className='input-search' onChange={onSearchChange} type='search' />
+      <button className='btn-search' onClick={callApi}>SEARCH MUSIC</button>
+
+      {data?.tracks?.items?.map((d) => (
         <SongComponent key={d.id} data={d} />
       ))}
     </div>
   );
-}
+};
 
 export default songComponent;
